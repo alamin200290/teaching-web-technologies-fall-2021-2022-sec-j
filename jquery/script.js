5a1c54f5b146f2a4609ce748ae6e2fd19f88cc9d
sarwar_sikder <amin.se.aiub@gmail.com>
2021-12-01 14:49:52 +0600

    Jquery basic

---
+++ jquery/script.js
@@ -0,0 +1,64 @@
+"use strict"
+
+//a =10;
+var b=20;
+let c=30;
+const d=50;
+
+let student = [1, 'alamin', 'alamin@aiub.edu', 2.4];
+
+student[0];
+
+/*
+function abc(){
+	return 0;
+}
+*/
+
+//alert("test");
+//console.log(student[1]);
+//document.write('<h1>this is JS action..</h1>');
+
+function f1(){
+	let element = document.getElementsByTagName('h1')[0];
+	element.innerHTML = "ALAMIN";	
+}
+
+function f2(){
+
+	let name = document.getElementById('name').value;
+	
+	if(name != ""){
+		document.getElementsByTagName('h1')[0].innerHTML = name;
+		//document.getElementById('name').value = "";
+		document.getElementById('d1').innerHTML = "";
+	}else{
+		document.getElementById('d1').innerHTML = "<p style='color:red'> Null data</p>";
+	}
+}
+
+
+function moveLeft(){
+
+	let current = document.getElementById('d1').style.left;
+	let num = current.replace('px','');
+	num	= parseInt(num)+10;
+	document.getElementById('d1').style.left = num+"px";
+}
+
+
+function moveRight(){
+
+	let current = document.getElementById('d1').style.left;
+	let num = current.replace('px','');
+	num	= parseInt(num)-10;
+	document.getElementById('d1').style.left = num+"px";
+}
+
+
+$('#d1').html('alamin');
+
+$('#mleft').click(function(){
+	alert($('#name').val());
+	$('#name').val('');
+})
